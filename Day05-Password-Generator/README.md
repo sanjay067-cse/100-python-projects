da9ee3f0ec1ca26caa27ace5b50c121d512a2e9f
sanjay067-cse <skumarskm2001@gmail.com>
2025-08-20 20:20:48 +0530

    Uploaded

---
+++ Day05-Password-Generator/README.md
@@ -0,0 +1,14 @@
+# Project 05 : 🔐Password Generator 
+
+Generates strong, random passwords based on:
+<br>
+🔸No. of letters
+<br>
+🔸No. of numbers
+<br>
+🔸No. of symbols
+<br>
+
+Uses randomization and character shuffling for maximum security.
+<br>
+Ensures each password is unique, secure, and tailored to user needs.
---
+++ Day05-Password-Generator/main.py
@@ -0,0 +1,33 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+# Empty list to store required no. of letters, numbers and symbols in the password
+password_list = []
+
+#Adding random letters, numbers and symbols in the password_list one by one
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for char in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+
+print(password_list)             #Will print password_list in the order: [letters, symbols, numbers] since these got added one by one in their respective for loop
+random.shuffle(password_list)    #Randomly shuffling the password_list for greater security using random.shuffle() function
+print(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your strong password is: {password}")
