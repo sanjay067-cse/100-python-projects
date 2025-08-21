01a236c4aba7cf3866e17cb3c8b974bf8c432995
sanjay067-cse <skumarskm2001@gmail.com>
2025-08-21 09:09:00 +0530

    Uploaded

---
+++ Day09-The-Secret-Auction/README.md
@@ -0,0 +1,5 @@
+# Project 09 : The Secret Auction
+
+🔸A python program that conducts a Secret Auction where multiple participants can place bids privately.
+<br>
+🔸After collecting all bids, it reveals the winner with the highest bid in a fair and confidential way.
---
+++ Day09-The-Secret-Auction/main.py
@@ -0,0 +1,30 @@
+import art
+print("\n********************** WELCOME TO THE SECRET AUCTION **********************")
+print(art.logo)    #printing logo from art.py
+
+# function to compare all the bids when all the bids have been done
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid :
+            highest_bid = bid_amount
+            winner= bidder
+
+    print(f"The Winner is {winner} with a bid of ${highest_bid}.")
+
+
+bids = {}      #Empty list to store names & bids of every person
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price        #Adding name and price of bid to the list every time a new bidder joins
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'. \n")
+    if should_continue == "no" :
+        continue_bidding = False
+        find_highest_bidder(bids)   #Function call to find out the highest bidder since no more further bids are to be done
+
+    elif should_continue == "yes" :
+        print("\n" * 50)            #Next bid will be done after a gap of 50 lines so that the next person is unable to see previous bids
